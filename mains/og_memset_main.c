228cca362b8a14fc2633df9981fa685a13e1f2a6
oguz <oguzharunb@gmail.com>
2024-09-26 16:07:08 +0300

    og_write_main and og_memset_main implemented

---
+++ mains/og_memset_main.c
@@ -0,0 +1,14 @@
+#include "oglib.h"
+
+#include <stdio.h>
+int	main(void)
+{
+	char	str[] = "hello world";
+	int	i;
+
+	og_memset(str, 'c', 7);
+	i = -1;
+	while(++i < (int)sizeof(str))
+		printf("str[%.2i] = %c = %i\n", i, (str[i] ? str[i] : '0'), str[i]);
+	return (0);
+}
---
+++ mains/og_write_main.c
@@ -0,0 +1,12 @@
+#include "oglib.h"
+#define BUF_SIZE 32
+
+int	main(void)
+{
+	char	buf[BUF_SIZE];
+	size_t	bytes;
+	
+	bytes = og_read(0, buf, BUF_SIZE);
+	og_write(1, buf, bytes);
+	return (0);
+}
