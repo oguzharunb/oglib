bd09452c62604a8ae044ba72f3f7e53e915d5cda
oguzharunb <oguzharundevp@protonmail.com>
2024-09-27 14:58:47 +0300

    modified

--- og_memccpy.c
+++ og_memccpy.c
@@ -1,27 +1,21 @@
 #include "oglib.h"
 
-
 void    *og_memccpy(void *dest, const void *src, int c, size_t n)
 {
-    size_t  i;
-    unsigned char *temp1;
-    unsigned char *temp2;
+	size_t  i;
+	unsigned char *temp1;
+	unsigned char *temp2;
 
-    // Cast src and dest to unsigned char* for byte-by-byte copy
-    temp1 = (unsigned char *)src;
-    temp2 = (unsigned char *)dest;
+	temp1 = (unsigned char *)src;
+	temp2 = (unsigned char *)dest;
 
-    for (i = 0; i < n; i++)
-    {
-        temp2[i] = temp1[i];
-
-        // If the character matches, return the next byte position
-        if (temp1[i] == (unsigned char)c)
-        {
-            return (temp2 + i + 1);
-        }
-    }
-
-    // If c is not found, return NULL
-    return NULL;
+	for (i = 0; i < n; i++)
+	{
+ 		temp2[i] = temp1[i];
+		if (temp1[i] == (unsigned char)c)
+		{
+			return (temp2 + i + 1);
+		}
+	}
+	return NULL;
 }
