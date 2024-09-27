7143d6f6bed17da59ac8f2da6bd52840578a490d
oguzharunb <oguzharundevp@protonmail.com>
2024-09-27 18:18:56 +0300

    code generator bugs fixed

--- set_prototypes.sh
+++ set_prototypes.sh
@@ -1,13 +1,21 @@
-# using: ./set_prototypes.sh header.h file1.c file2.c file3.c
-# this file is responsible to generate code.
-# i dont want to enter each prototype to oglib.h one by one.
-# script searches in given files and look for the lines that starts with void, int, char etc.
-# Puts their prototype in the header (replaces //prototypes line with actual prototypes)
+#!/bin/bash
 
-# put a line starts with //prototypes so script could work properly
-function_types='^\(void\|int\|char\|size_t\)' 
+# using: ./set_prototypes.sh header.h file1.c file2.c file3.c
+# This script automatically extracts function prototypes from given C files
+# and inserts them into the specified header file.
+# The function types (void, int, etc.) are read from config.json.
+
+# Read function types from config.json
+config_file="config.json"
+function_types=$(jq -r '.function_types | join("|")' "$config_file")
+
+# Regex pattern for matching function prototypes only (functions have parentheses after their names)
+
 header=$1
 shift
-prototypes=$(grep -h '^\(void\|int\|char\|size_t\)'  "$@" | sed 's/$/;/') #filenames should be given as parameters
-echo ${prototypes}
-perl -pi -e 's|//prototypes|'"$prototypes"'|' $header #header name should be given as parameter
+# Extract lines, clean them up, and filter for function prototypes
+prototypes=$(grep -hE "^\s*($function_types)\s+[^;]*\([^\)]*\)\s*\{?" "$@" | sed -E 's/^\s*//; s/\s*\{.*$/;/; s/$/;/')
+echo $prototypes
+# Replace //prototypes in the header file with the extracted prototypes
+perl -pi -e 's|//prototypes|'"$prototypes"'|' "$header"
+
