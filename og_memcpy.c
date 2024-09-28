161da4d5b32d7922afe7b368b90d9d4208cbe527
oguzharunb <oguzharundevp@protonmail.com>
2024-09-28 12:27:28 +0300

    it was not coping the last byte

--- og_memcpy.c
+++ og_memcpy.c
@@ -4,13 +4,13 @@ void	*og_memcpy(void *dest, const void *src, size_t n)
 {
 	char	*ptr1;
 	char	*ptr2;
+	size_t	i;
 
 	ptr1 = (char *)dest;
 	ptr2 = (char *)src;
-
-	while(n-- > 0)
-		ptr1[n] = ptr2[n];
-	ptr1[n] = ptr2[n];
+	i = -1;
+	while(++i < n)
+		ptr1[i] = ptr2[i];
 	return (ptr1);
 }
 
