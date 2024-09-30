9be958ccbf213d7c95b3ecaa49cfee40c039d6b9
oguzharunb <oguzharundevp@protonmail.com>
2024-09-30 18:10:51 +0300

    t_list struct added

--- prototype.h
+++ prototype.h
@@ -3,6 +3,13 @@
 # include <stddef.h>
 # include <stdlib.h>
 
+typedef struct	s_list
+{
+	void		*content;
+	struct	s_list	*next;
+}	t_list;
+
+
 # include <ctype.h>
 # include <string.h>
 # include <unistd.h>
